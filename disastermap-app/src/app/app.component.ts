00e6e25f161788cb155fcaa71df12e512607ccbf
eyhhuang <emily.huang1@hotmail.com>
2018-09-08 03:09:02 -0400

    added countries to overlay

--- disastermap-app/src/app/app.component.ts
+++ disastermap-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import {MapService} from './map.service';
-declare var google: any;
+import {longLat, CountryCode} from '../assets/longitude-lagitude'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +11,12 @@ export class AppComponent {
   title: string = 'My first AGM project';
   lat: number = 51.678418;
   lng: number = 7.809007;
+  longLat = longLat;
   // item: any = {lat: 56.1304, lon: 106.3468};
-  latLonArray: any[] = [{lat: 56.1304, lon: -106.3468}];
+  latLonArray: number[][] = [[56.1304, -106.3468],[50,-6] ];
   ngOnInit(){
+    console.log(longLat);
     console.log(this.latLonArray);
-    // console.log(this.item);
   }
 }
 
