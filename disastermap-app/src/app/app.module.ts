24da0bc7e401116a16c78feca663adc18310f36e
eyhhuang <emily.huang1@hotmail.com>
2018-09-07 22:41:56 -0400

    showed map

--- disastermap-app/src/app/app.module.ts
+++ disastermap-app/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
+import { AgmOverlays } from "agm-overlays";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmOverlays,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDjLrZaifz5VwkGtWLCu296LkxC8vIX2kY'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
